3f580a0ec70a307c06b158156897e163c6124020
sivaiah1525 <sivaganga821@gmail.com>
2020-06-06 13:51:28 +0530

    add in maill send to forgotpassword

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = express();
 const cors = require('cors')
 const port = 5000;
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+var nodemailer = require('nodemailer');
 const jwt = require('jsonwebtoken');
 var secret = 'fe1a1915a379f3be5394b64d14794932';
 const db = require('./models');
@@ -22,7 +23,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // CREAT USER NEW
 app.post('/user/creat', async function(req, res) {
     try {
-        // const salt = await bcrypt.genSalt()
         const hashpassword = await bcrypt.hash(req.body.password, 10)
         const user = { name: req.body.name, mailId: req.body.mailId, password: hashpassword }
         const newUser = new creatUser(user);
@@ -32,6 +32,7 @@ app.post('/user/creat', async function(req, res) {
         res.status(400).send('bad request');
     }
 })
+
 
 // LOGIN USERE
 
@@ -61,8 +62,8 @@ app.post('/user/login', function(req, res) {
 
 })
 
-// STUDENTES 
-// creat 
+
+// creat newstudent
 app.post('/user/student', async function(req, res) {
     try {
         const student = req.body
@@ -74,7 +75,7 @@ app.post('/user/student', async function(req, res) {
     }
 })
 
-// get all 
+// get all STUDENTES
 app.get('/user/student/all', async function(req, res) {
         try {
             var skip = Number(req.query.skip)
@@ -87,7 +88,7 @@ app.get('/user/student/all', async function(req, res) {
             res.status(400).send('bad request');
         }
     })
-    //  DeletById 
+    //  DeletById student
 app.post('/user/student/deletbyid', async(req, res) => {
     try {
         const result = await Student.findByIdAndDelete({ _id: req.body.id }).lean();
@@ -97,7 +98,7 @@ app.post('/user/student/deletbyid', async(req, res) => {
     }
 })
 
-// updateByID 
+// updateByID student
 app.put('/user/student/updatebyid', async(req, res) => {
     try {
         const updateid = { _id: req.body.id };
@@ -118,5 +119,58 @@ app.put('/user/student/updatebyid', async(req, res) => {
         console.log(error)
     }
 })
+
+
+// Reset password verification User send maill link
+app.post('/resetpassword', async(req, res) => {
+    var mailId = req.body.mailId
+    await creatUser.findOne({ mailId: mailId }).then((user) => {
+        console.log(user)
+        if (!user) {
+            return res.status(400).json({ msg: "User not find" })
+        } else {
+            jwt.sign({ id: user._id }, secret, (error, token) => {
+                if (!token) {
+                    res.send(400).json({ msg: "error in token" })
+                } else {
+                    var transporter = nodemailer.createTransport({
+                        host: 'smtp.gmail.com',
+                        port: 465,
+                        secure: true,
+                        auth: {
+                            user: 'sivagopi821@gmail.com',
+                            pass: 'mech@537'
+                        }
+                    });
+                    var mailOptions = {
+                        from: 'sivagopi821@gmail.com',
+                        to: mailId,
+                        subject: 'Reset your account password',
+                        html: '<h4><b>Reset Password</b></h4>' +
+                            '<p>To reset your password, complete this form:</p>' + '<a href="http://localhost:4200/resetpassword">Resetpassword Link</a>'
+                    };
+                    transporter.sendMail(mailOptions, function(error, info) {
+                        if (info) {
+                            res.json({ msg: " maill send" })
+                        } else {
+                            res.json({ msg: " mail not send " })
+                        }
+                        transporter.close();
+                    });
+                }
+            })
+        }
+
+    })
+})
+
+// resetPassword update user 
+app.post('/resetpassword/update', async(req, res) => {
+    const hashresetpassword = await bcrypt.hash(req.body.Cfmpassword, 10)
+
+
+})
+
+
 
 app.listen(port, console.log(`app is running @ port number ${port}`))
